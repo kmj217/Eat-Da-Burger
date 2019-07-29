f2d9c7be1dc6b3f3f934f6f17e7e7d8fd01e299c
KMJ217 <kmj217@hotmail.com>
2019-07-28 21:08:52 -0400

    created burgerController.js

---
+++ controllers/burgersController.js
@@ -0,0 +1,18 @@
+// Dependencies
+var express = require("express");
+
+// Create the router for the app, and export the router at the end of your file.
+var router = express.Router();
+// Create routes and set up logic where required.
+router.get("/", function (req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgerController.js");
+var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
